748ee42a503d45f02d35ee68f2a8bcc14c205d7f
Lavinia Bianca <chelaru_laviniabianca@yahoo.com>
2023-10-27 15:27:57 +0200

    Add npx webpack --watch

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,8 @@ module.exports = {
         filename:'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    watch: true,
+    watchOptions: {
+        ignored: './node_modules',
+      },
 };
